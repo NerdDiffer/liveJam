e341f197d2fdb10263a76f45b6310dfd58b49c88
Rafael Espinoza <nerddiffer@gmail.com>
2016-08-09 18:16:11 -0700

    Fix db configuration

--- server/db/init.sh
+++ server/db/init.sh
@@ -2,11 +2,12 @@
 
 DB='liveJam_development'
 DB_USER='liveJam'
+DB_PASSWORD=''
 
 sql="
   DROP DATABASE IF EXISTS $DB;
   CREATE DATABASE $DB;
-  CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '';
+  CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';
   GRANT ALL PRIVILEGES ON $DB.* TO '$DB_USER'@'localhost';
   FLUSH PRIVILEGES;
 "
